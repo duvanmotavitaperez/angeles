e121be9460c4c40766595a84196525269fba76e6
duvanmotavitaperez <dmotavita@uninorte.edu.co>
2021-10-19 16:57:22 -0500

    menu config

--- app.py
+++ app.py
@@ -2,9 +2,9 @@ from flask import Flask, render_template, url_for, send_from_directory
 import os
 
 app = Flask(__name__)
-path_fonts = os.path.abspath('./fonts/')
 @app.route('/')
 def main():
     return render_template('angeles.html')
 
-app.run(debug=True, host='192.168.1.8')
+app.run(debug=True, host='192.168.10.189')
+input()
--- static/js/code.js
+++ static/js/code.js
@@ -1,11 +1,61 @@
+//Copiu
 window.onload = () => {
-
-   const $itemMove1 = document.querySelectorAll('.container-move')[0]
-   const $itemMove2 = document.querySelectorAll('.container-move')[1]
-   const $itemMove3 = document.querySelectorAll('.container-move')[2]
+   document.addEventListener('keyboard', (event) => {
+      console.log(event.key)
+})
    const $containerFixed = document.querySelector('.container-fixed')
+   window.innerWidth >= 1500 ?
+      $containerFixed.innerHTML += `<div class='container-move'>${document.querySelectorAll('.container-move')[1].innerHTML}</div>`  
+      : null
+   let $itemMove0 = document.querySelectorAll('.container-move')[0]
+   let $itemMove1 = document.querySelectorAll('.container-move')[1]
+   let $itemMove2 = document.querySelectorAll('.container-move')[2]
+   let $itemMove3 = document.querySelectorAll('.container-move')[3] || null
+   let $menuButton = document.querySelector('i.menu-icon')
+   let $ul = document.querySelector('menu ul')
+   let moveIcon = true
 
-   const oneBound = $itemMove1.getBoundingClientRect().x
+   //This is the effect in the menu
+   $menuButton.addEventListener('click', () => {
+        var count = 14.9
+        moveIcon 
+        ? !function() {
+              let timeLeft = setInterval(()=> {
+                                 $ul.style.transform = `translate(${count}rem)`
+                                 count -= .2
+                                 if(count <= 0){
+                                    clearInterval(timeLeft)
+                                    count = 0
+                                 }
+                              }, 1)
+               $ul.focus()
+           }() 
+        : !function() {
+             let coun = .1
+             let timeRight = setInterval(()=> {
+                                 $ul.style.transform = `translate(${coun}rem)`
+                                 coun += .2
+                                 if(coun >= 14.9){
+                                    clearInterval(timeRight)
+                                    coun = 14.9
+                                 }
+                              }, 1) 
+            }() 
+      moveIcon = moveIcon ? false : true
+   })
+   $ul.addEventListener('blur', () => {
+      alert('test')
+             let coun = .1
+             let timeRight = setInterval(()=> {
+                                 $ul.style.transform = `translate(${coun}rem)`
+                                 coun += .2
+                                 if(coun >= 14.9){
+                                    clearInterval(timeRight)
+                                    coun = 14.9
+                                 }
+                              }, 1) 
+            })
+   const oneBound = $itemMove0.getBoundingClientRect().x
 
    let count = 0
    let conf = true
@@ -15,17 +65,19 @@ window.onload = () => {
    
    setInterval(()=> {
       if(conf){
-         const threeBound = $itemMove3.getBoundingClientRect().x
+         let threeBound = $itemMove2.getBoundingClientRect().x
          if(threeBound <= oneBound){
+         $itemMove0.style.transform = `translate(${0}px)`
          $itemMove1.style.transform = `translate(${0}px)`
          $itemMove2.style.transform = `translate(${0}px)`
-         $itemMove3.style.transform = `translate(${0}px)`
+         $itemMove3 ? $itemMove3.style.transform = `translate(${0}px)` : null
          count = 0
          }
          else{
+            $itemMove0.style.transform = `translate(-${count}px)`
             $itemMove1.style.transform = `translate(-${count}px)`
             $itemMove2.style.transform = `translate(-${count}px)`
-            $itemMove3.style.transform = `translate(-${count}px)`
+            $itemMove3 != null ? $itemMove3.style.transform = `translate(-${count}px)` : null
             count += 0.1
          }
       }
