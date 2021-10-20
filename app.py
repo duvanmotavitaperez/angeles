1f2ddd16dbe0969db5fde4c901f8db610608b62e
duvanmotavitaperez <dmotavita@uninorte.edu.co>
2021-10-20 10:59:30 -0500

    contain admin's page

--- app.py
+++ app.py
@@ -1,10 +1,21 @@
-from flask import Flask, render_template, url_for, send_from_directory
+from flask import Flask, render_template, url_for, send_from_directory, request
 import os
 
+user = 'angeles'
+password = 'Sudomkdir5914'
+
 app = Flask(__name__)
+
 @app.route('/')
 def main():
-    return render_template('angeles.html')
+    return render_template('admin.html')
 
+@app.route('/main', methods=['POST', 'GET'])
+def angeles():
+    if request.method == 'GET':
+       return render_template('admin.html')
+    else:
+        if user == request.form['user']:
+            return render_template('angeles.html')
+        return render_template('admin.html')
 app.run(debug=True, host='192.168.10.189')
-input()
