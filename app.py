f05774530aaa47459aaf1f65eb76b3c3de39026b
duvanmotavitaperez <dmotavita@uninorte.edu.co>
2021-10-22 08:48:44 -0500

    last test today

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template, url_for, send_from_directory, request
+from flask import Flask, render_template, url_for, send_from_directory, request, make_response
 import os
+import socket
 
 user = 'angeles'
 password = 'Sudomkdir5914'
@@ -8,14 +9,24 @@ app = Flask(__name__)
 
 @app.route('/')
 def main():
+    cof_log = request.cookies.get('login', 'tolo')
+    print(cof_log)
     return render_template('admin.html')
 
 @app.route('/main', methods=['POST', 'GET'])
 def angeles():
     if request.method == 'GET':
-       return render_template('admin.html')
+        sess= make_response(render_template('admin.html'))
+        sess.set_cookie('login', )
+        sess.set_cookie('sesskey', )
+        return sess
     else:
         if user == request.form['user']:
             return render_template('angeles.html')
         return render_template('admin.html')
-app.run(debug=True, host='192.168.10.189')
+
+@app.errorhandler(404)
+def error404(e):
+    return render_template('404.html')
+
+app.run(debug=True, host=f'{socket.gethostname()}')
--- static/js/admin.js
+++ static/js/admin.js
@@ -1,11 +1,22 @@
 window.onload = () => {
-    let $form = document.getElementById('formdata')
-    let $button = document.getElementById('button')
-    let $textArea = document.querySelector('textarea')
-    $button.addEventListener('click', () => {
-        console.log($textArea.value)
+    const $form = document.getElementById('formdata')
+    const $button = document.getElementById('button')
+    const $eye = document.getElementById('showText')
+    const $textArea = document.querySelector('textarea')
+    const $passArea = document.querySelector('.input-item.password')
+    const $buttonSend = document.querySelector('.button-send')
+    
+    $eye.addEventListener('click', () => {
+        $eye.classList.toggle('show-text')
+    })
+    $buttonSend.addEventListener('click', () => {
+        form = new FormData($form)
+        const username = form.get('username')
+        const pass = form.get('userpass')
+        
     })
     $form.addEventListener('submit', (event) => {
         event.preventDefault()
     })
 }
+//This is a test
